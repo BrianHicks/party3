18d32b3f596f8878b52ed8bf6e861cf4461f9a64
Brian Hicks <brian.hicks@rockfishinteractive.com>
2012-10-22 18:57:04 -0500

    add REDISTOGO_URL lookup

--- party3/celery.py
+++ party3/celery.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
+import os
 
 from celery import Celery
 
 celery = Celery(
     'party3.celery',
-    broker='redis://',
-    backend='redis://',
+    broker=os.environ.get('REDISTOGO_URL', 'redis://'),
+    backend=os.environ.get('REDISTOGO_URL', 'redis://'),
     include=['party3.supplies']
 )
 
